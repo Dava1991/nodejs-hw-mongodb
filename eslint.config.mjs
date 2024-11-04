08093f2fe4c070db8d570d4a2f27bfb0f7a512f0
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2024-11-04 20:11:21 +0200

    4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,21 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
     files: ['src/**/*.js'],
     languageOptions: { globals: globals.node },
     rules: {
       semi: 'error',
-      'no-unused-vars': ['error', { args: 'none' }],
+      'no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',
+        },
+      ],
       'no-undef': 'error',
     },
   },
---
+++ src/services/contacts.js
@@ -0,0 +1,41 @@
+import { ContactsCollection } from '../db/models/contacts.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+
+  return contacts;
+};
+
+export const getContactById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const newContact = await ContactsCollection.create(payload);
+
+  return newContact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const updatedContact = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    { new: true, includeResultMetadata: true, ...options },
+  );
+
+  if (!updatedContact || !updatedContact.value) return null;
+
+  return {
+    contact: updatedContact.value,
+    isNew: updatedContact?.lastErrorObject?.upserted,
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const deletedContact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return deletedContact;
+};
