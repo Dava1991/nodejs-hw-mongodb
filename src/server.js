5fd2c512f54c1b9ee3974a38d1e3bf8ca568cc11
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2024-11-15 21:48:57 +0200

    4

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
+import router from './routers/index.js';
 
-import StudentsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -14,7 +15,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
-
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
@@ -23,7 +24,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(StudentsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
