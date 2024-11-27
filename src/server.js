bc7abfdc1bcdae3207a596a37142a9e2c8ec9d24
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2024-11-27 15:48:52 +0200

    5

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -24,7 +25,7 @@ export const setupServer = () => {
       },
     }),
   );
-  
+
   app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use(router);
@@ -37,3 +38,6 @@ export const setupServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
+
+ app.use('/uploads', express.static(UPLOAD_DIR));
+ app.use('/api-docs', swaggerDocs());
